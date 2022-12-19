3e93dde6ea6faf72e8e50dafee75e6e057a8d764
Matt Rangel <matt333three@aol.com>
2022-12-19 13:11:32 -0600

    Create javascript to go with basic layout and css transition

--- game.js
+++ game.js
@@ -1,38 +1,43 @@
-const playChoices = ["Rock","Paper","Scissors"];
+const PLAY_CHOICES = ["Rock","Paper","Scissors"];
+const ROUND_OUTCOMES = ["Player wins", "Computer wins", "Tie"]
+const NUM_OF_ROUNDS = 5;
+let currentRound = 0;
+let scores;
 
 function getComputerChoice() {
-	return playChoices[Math.floor(Math.random()*3)];
+	return PLAY_CHOICES[Math.floor(Math.random()*3)];
 }
 
-function getPlayerChoice() {
-	let choice = prompt("Rock, Paper, or Scissors?");
-	choice = (choice !== null) ? choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase() : "";
-	if (playChoices.indexOf(choice) === -1) {
-		alert("Invalid input.\nDefaulting to rock...");
-		choice = "Rock";
-	}
-	return choice;
-}
-
-function playRound(playerSelection, computerSelection) {
-	let result = (playerSelection === computerSelection) ? "Tie" :
-	(playerSelection === "Rock" && computerSelection === "Scissors" || playerSelection === "Paper" && computerSelection === "Rock" || playerSelection === "Scissors" && computerSelection === "Paper") ? "Player" : "Computer";
-	//return `${playerSelection} vs ${computerSelection}: ${result}`;
+function evaluateRound(playerSelection, computerSelection) {
+	let result = (playerSelection === computerSelection) ? ROUND_OUTCOMES[2] :
+	(playerSelection === "Rock" && computerSelection === "Scissors" || playerSelection === "Paper" && computerSelection === "Rock" || playerSelection === "Scissors" && computerSelection === "Paper") ? ROUND_OUTCOMES[0]: ROUND_OUTCOMES[1];
 	return result;
 }
 
-function game() {
-	let computerPoints = 0;
-	let playerPoints = 0;
-	for (let i = 0; i < 5; i++) {
-		const result = playRound(getPlayerChoice(),getComputerChoice());
-		if (result === "Player") { playerPoints += 1; }
-		else if (result === "Computer") { computerPoints += 1; }
-		console.log(`Round ${i}: ${result}`);
-	}
-	let winState = (playerPoints > computerPoints) ? "Won" :
-	(playerPoints < computerPoints) ? "Lost" : "Tied";
-	
-	return `You ${winState}, with a score of ${playerPoints} to ${computerPoints}!`;
-	//return winState;
+function runRound(playerSelection) {
+	let roundEnd = (++currentRound === NUM_OF_ROUNDS);
+	if (currentRound === 1) { scores = { computer:0, player:0 }; }
+	let computerSelection = getComputerChoice();
+	let roundOutcome = evaluateRound(playerSelection, computerSelection);
+	if (roundOutcome === "Player wins") { ++scores.player; }
+	else if (roundOutcome === "Computer wins") { ++scores.computer; }
+	updateHTML(roundOutcome, playerSelection, computerSelection, roundEnd); 
+	if (roundEnd) { currentRound = 0; }
 }
+
+function updateHTML(roundOutcome, playerChoice, compChoice, roundEnd) {
+	const scoreCard = document.querySelector("#score-card");
+	const winnerCard = document.querySelector("#winner-card");
+	const outcomeCard = document.querySelector("#round-outcome");
+	scoreCard.textContent = `Player: ${scores.player} | Computer: ${scores.computer}`
+	outcomeCard.textContent = `Round ${currentRound}/${NUM_OF_ROUNDS}: ${playerChoice} vs ${compChoice} (${roundOutcome})`
+	outcomeCard.classList.add(["win","lose","tie"][ROUND_OUTCOMES.indexOf(roundOutcome)]);
+	if (roundEnd) {
+		winnerCard.textContent = "Game over, you " + ((scores.player > scores.computer) ? "won!" :
+		(scores.player < scores.computer) ? "lost!" : "tied!");
+	} else { winnerCard.textContent = ""; }
+}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => button.addEventListener("click", () => runRound(button.textContent)));
+document.querySelector("#round-outcome").addEventListener("transitionend",(e) => e.target.classList.remove("win","tie","lose")); 
